b63b07be38f538d9518ee3e543c77b6d4b977653
Yusuf Budak <yusufemrebudak16525@gmail.com>
2025-11-29 14:36:59 +0300

    first commit

---
+++ backend/src/main/java/com/yusufbudak/habittracker/HabittrackerApplication.java
@@ -0,0 +1,13 @@
+package com.yusufbudak.habittracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabittrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabittrackerApplication.class, args);
+	}
+
+}
